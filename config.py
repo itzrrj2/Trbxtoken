f2d27b75b641c42cc1c758b67bf8e896326e32a0
Itzrrj <shresthxraj@gmail.com>
2024-12-20 02:03:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://shresthxraj:apple@telebotz.i5c1l.mongodb.net/?retryWrites=true&w=majority&")
 DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 
 #Shortner (token system) 
@@ -10,7 +10,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "30beec9d608c3a665efea26f0d0fb14f32b11281")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "46790105f2ff6a9c33e0bdf12a6f4e01fbde48b3")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://your.itz-ashlynn.workers.dev/?start=MzA2OTMxOTgxMzI2MzkwOTAwLzczMTI4MjAxNg&bot=AR_File_To_Link_Bot") # shareus ka tut_vid he 
