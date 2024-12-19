e2714b6b915f935fd147decd4c58b90b0bb4673f
Itzrrj <shresthxraj@gmail.com>
2024-12-20 03:07:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://cphdlust:cphdlust@cphdlust.ydeyw.mongodb.net/?retryWrites=true&w=majority&")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://cphdlust:cphdlust@cphdlust.ydeyw.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 
 #Shortner (token system) 
