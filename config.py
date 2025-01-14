c05de0cfbe102f69a47db6db3100522a33f83a11
Itzrrj <shresthxraj@gmail.com>
2025-01-15 00:43:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 # check my discription to help by using my refer link of shareus.io
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shortner.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "6fd47c5091eb04ef85f668af5fabe5c72b0c89c4")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "46790105f2ff6a9c33e0bdf12a6f4e01fbde48b3")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://your.itz-ashlynn.workers.dev/?start=MzA2OTMxOTgxMzI2MzkwOTAwLzczMTI4MjAxNg&bot=AR_File_To_Link_Bot") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/srbots_tutorial") # shareus ka tut_vid he 
