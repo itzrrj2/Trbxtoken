221767e03fad44ccf16ad93236717b56dcec512d
Itzrrj <shresthxraj@gmail.com>
2025-01-15 15:57:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "46790105f2ff6a9c33e0bdf12a6f4e01fbde48b3")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/srbots_tutorial") # shareus ka tut_vid he 
